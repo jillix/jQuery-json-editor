694eb720e347020210e4ec1504a6d24d94d38b6b
Ionica Bizau <bizauionica@yahoo.com>
2013-08-01 13:07:38 +0300

    Module logic.

--- main.js
+++ main.js
@@ -6,5 +6,29 @@ module.exports = function(config) {
     var self = this;
     Events.call(self, config);
 
-    // TODO
+    $(self.dom).on("submit", "form", function (e) {
+
+        e.preventDefault();
+
+        var $form = $(this);
+        var serializedForm = {};
+
+        $form.find("[data-field]").each(function () {
+            var $element = $(this);
+            var how = $element.attr("data-value") || "val";
+            var params = $element.attr("data-params");
+            var field = $element.attr("data-field");
+            var value;
+
+            if (!params) {
+                value = $element[how]();
+            } else {
+                value = $element[how](params);
+            }
+
+            serializedForm[field] = value;
+        });
+
+        self.emit(config.eventName || "serializedForm", serializedForm);
+    });
 };
