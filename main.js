ab4ed65bf01ec2e30fc2dc1f6d3f9743f6bcda20
Ionica Bizau <bizauionica@yahoo.com>
2013-08-01 11:14:53 +0300

    Add main.js and mono.json.

---
+++ main.js
@@ -0,0 +1,10 @@
+var Bind = require("github/jillix/bind");
+var Events = require("github/jillix/events");
+
+module.exports = function(config) {
+
+    var self = this;
+    Events.call(self, config);
+
+    // TODO
+};
