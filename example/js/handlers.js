a661c68d8f99809acae5052a6e9c2507481706f5
Ionică Bizău <bizauionica@yahoo.com>
2014-11-18 21:31:49 +0200

    Create form from input daa

--- example/js/handlers.js
+++ example/js/handlers.js
@@ -1,57 +1,11 @@
 $(function () {
-    var firstForm = $(".first-form").jsonEdit({
-        data: {
-            age: 19,
-            country: "Romania",
-            name: {
-                first: "Johnny",
-                last: "B."
-            },
-            student: true,
-            hobbies: ["piano", "blogging"],
-            born: new Date(1995, 9, 14),
-            keyboars: [
-                {
-                    manufacturer: "Yamaha",
-                    type: "PSR8000",
-                    pianoKeys: false,
-                    digital: true
-                },
-                {
-                    manufacturer: "Yamaha",
-                    type: "P95",
-                    pianoKeys: true,
-                    digital: true
-                }
-            ]
-        },
-        schema: {
-            age: { label: "Age" },
-            country: { label: "Country" },
-            name: {
-                label: " ",
-                schema: {
-                    first: { label: "First Name" },
-                    last: { label: "Last Name" }
-                }
-            },
-            student: { label: "Student" },
-            hobbies: { label: "Hobbies" },
-            born: { label: "Born" },
-            keyboars: {
-                label: "Keyboards",
-                schema: {
-                    manufacturer: { label: "Manufacturer" },
-                    type: { label: "Type" },
-                    pianoKeys: { label: "Piano Keys" },
-                    digital: { label: "Digital" },
-                }
-            },
-            born: { label: "Born Date" }
-        }
-    });
-
-    $("button").on("click", function () {
+    var firstForm = null;
+    $("#updateForm").on("click", function () {
+        firstForm = $(".first-form").empty().jsonEdit(
+            eval($("#inputData").val())
+        );
+    }).click();
+    $("button#getData").on("click", function () {
         $("pre").html(JSON.stringify(firstForm.getData(), null, 4));
     });
 });
